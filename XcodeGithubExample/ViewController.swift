91229ceadc30fd97237f6c389ba1c45faca4e812
BobZhang <bobzhang@BobZhangtekiMacBook-Pro.local>
2014-12-04 12:01:42 +0800

    Initial Commit

---
+++ XcodeGithubExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XcodeGithubExample
+//
+//  Created by BobZhang on 14/12/4.
+//  Copyright (c) 2014年 BobZhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
